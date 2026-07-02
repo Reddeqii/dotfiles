0084e253d45815711a345cee7344fb3618a25032
Reddeqii <liammurdock77@gmail.com>
2026-07-02 16:44:55 +0100

    Update launcher.sh

--- Scripts/launcher.sh
+++ Scripts/launcher.sh
@@ -10,6 +10,6 @@ choice=$(printf "Apps\nFile Browser" | rofi -dmenu -p "Launcher")
 case $choice in
 	"Apps") rofi -show drun > /dev/null 2>&1 &
 	;;
-        "File Browser") kitty -e fzf > /dev/null 2>&1 &
+    "File Browser") kitty -e fzf > /dev/null 2>&1 &
 	;;
 esac
