c5e54bfb5029f4f64c6abd863cc52d4549aa46c4
Reddeqii <liammurdock77@gmail.com>
2026-07-02 12:45:31 +0100

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+choice=$(printf "Apps\nFile Browser" | rofi -dmenu -p "Launcher")
+
+case $choice in
+	"Apps") rofi -show drun > /dev/null 2>&1 &
+	;;
+        "File Browser") kitty -e fzf > /dev/null 2>&1 &
+	;;
+esac
